a00a19d61a3af8a6ad3da11170aaf7cf200c11af
RuslanAmraev <beardedsmellyfox@gmail.com>
2021-09-14 17:35:56 +0600

    goods-slider-v0.1

--- script.js
+++ script.js
@@ -27,42 +27,40 @@ function ChooseCategory(){
     event.target.classList.add('goods-nav_el-active')
 }
 
-function ourWorkSlider(orientation){
 
-    const line = document.getElementsByClassName('our-works_carousel_el')
+
+function ourWorkSlider(orientation, elSelector, lineSelector, wrapperSelector){
+    const lineEls = document.getElementsByClassName(elSelector)
+    const line = document.getElementById(lineSelector)
     let lineLength = 0
-    let widhtOfThreeEl = 0
-    for (let i = 0; i < 3; i++) {
-        widhtOfThreeEl+= line[i].offsetWidth
-    }
-    for (let i = 0; i < line.length; i++) {
-        lineLength+=line[i].offsetWidth
-    }
+    let widthToMove = document.getElementById(wrapperSelector).offsetWidth
 
-    lineLength+=line.length * 20
-    widhtOfThreeEl+= 60
+    for (let i = 0; i < lineEls.length; i++) {
+        lineLength+=lineEls[i].offsetWidth
+    }
     
-   
-    let from = document.getElementById('our-work').style.transform.indexOf('(')+1
-    let to = document.getElementById('our-work').style.transform.indexOf(')')-2
-    let currentTranslate = document.getElementById('our-work').style.transform.slice(from, to) - 0
+    let currentTranslate = getTransformValue(line)
     
     if(orientation == 'prev'){
-        const a = widhtOfThreeEl + currentTranslate
+        const a = widthToMove + currentTranslate
         if(a > 0){
             return
         }
-        document.getElementById('our-work').style.transform = 'translateX('+ a +'px)'
+        console.log('Двигаю на ', a)
+        line.style.transform = 'translateX('+ a +'px)'
     }
     if(orientation == 'next'){
-        const a = currentTranslate - widhtOfThreeEl
-        if(Math.abs(a) > lineLength - 100){
+        const a = currentTranslate - widthToMove
+        if(Math.abs(a) > lineLength - 20){
             return
         }
-        document.getElementById('our-work').style.transform = 'translateX('+ a +'px)'
+        console.log('Двигаю на ', a)
+        line.style.transform = 'translateX('+ a +'px)'
     }
+}
 
-    from = document.getElementById('our-work').style.transform.indexOf('(')+1
-    to = document.getElementById('our-work').style.transform.indexOf(')')-2
-    currentTranslate = document.getElementById('our-work').style.transform.slice(from, to) - 0
+function getTransformValue(el){
+    let from = el.style.transform.indexOf('(')+1
+    let to = el.style.transform.indexOf(')')-2
+    return(el.style.transform.slice(from, to) - 0)
 }
