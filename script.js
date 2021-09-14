224618f3e46da04d3300108027397942885e2f4b
RuslanAmraev <beardedsmellyfox@gmail.com>
2021-09-14 12:40:06 +0600

    Our-works-slider v0.1

--- script.js
+++ script.js
@@ -26,3 +26,43 @@ function ChooseCategory(){
     }
     event.target.classList.add('goods-nav_el-active')
 }
+
+function ourWorkSlider(orientation){
+
+    const line = document.getElementsByClassName('our-works_carousel_el')
+    let lineLength = 0
+    let widhtOfThreeEl = 0
+    for (let i = 0; i < 3; i++) {
+        widhtOfThreeEl+= line[i].offsetWidth
+    }
+    for (let i = 0; i < line.length; i++) {
+        lineLength+=line[i].offsetWidth
+    }
+
+    lineLength+=line.length * 20
+    widhtOfThreeEl+= 60
+    
+   
+    let from = document.getElementById('our-work').style.transform.indexOf('(')+1
+    let to = document.getElementById('our-work').style.transform.indexOf(')')-2
+    let currentTranslate = document.getElementById('our-work').style.transform.slice(from, to) - 0
+    
+    if(orientation == 'prev'){
+        const a = widhtOfThreeEl + currentTranslate
+        if(a > 0){
+            return
+        }
+        document.getElementById('our-work').style.transform = 'translateX('+ a +'px)'
+    }
+    if(orientation == 'next'){
+        const a = currentTranslate - widhtOfThreeEl
+        if(Math.abs(a) > lineLength - 100){
+            return
+        }
+        document.getElementById('our-work').style.transform = 'translateX('+ a +'px)'
+    }
+
+    from = document.getElementById('our-work').style.transform.indexOf('(')+1
+    to = document.getElementById('our-work').style.transform.indexOf(')')-2
+    currentTranslate = document.getElementById('our-work').style.transform.slice(from, to) - 0
+}
